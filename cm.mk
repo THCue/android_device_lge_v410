debb2e0181ad5d98ae167ae5afc76a5fa4c58118
Zane <zane.stewart@gmail.com>
2015-02-18 21:02:54 -0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -3,20 +3,20 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/du/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := v410
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/v410/device_v410.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := v410
-PRODUCT_NAME := cm_v410
+PRODUCT_NAME := du_v410
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := v410
 PRODUCT_MANUFACTURER := lge
